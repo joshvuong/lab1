81b3ac31a92d5f355a5ec34ae97e700abfa4483a
Joshua Vuong <jtvuong@ualberta.ca>
2023-09-13 21:43:43 -0600

    updated script

--- lab1.py
+++ lab1.py
@@ -2,6 +2,9 @@ import requests
 
 print(requests.__version__)
 
-resp = requests.get("http://google.com")
+resp = requests.get("https://raw.githubusercontent.com/joshvuong/lab1/master/lab1.py")
+
+f = open('file1.txt', 'w')
+f.write(resp.text)
 
 print(resp.text)
